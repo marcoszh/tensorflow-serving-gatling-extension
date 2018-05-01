b72b5e6d634bcbb720173fb8d85e9f9006130a85
ZHANG Chengliang <czhangbn@connect.ust.hk>
2018-05-01 22:26:10 +0800

    mul test

--- locust_file.py
+++ locust_file.py
@@ -19,14 +19,17 @@ class GrpcLocust(Locust):
  
 class ApiUser(GrpcLocust):
      
-    min_wait=100    # Min time between requests of each user
-    max_wait=200    # Max time between requests of each user
+    min_wait=0    # Min time between requests of each user
+    max_wait=100    # Max time between requests of each user
      
  
     class task_set(TaskSet):
 
         def on_start(self):
             print "on start"
+
+        def on_stop(self):
+            print "on stop"
  
         @task
         def get_prediction(self):
@@ -35,20 +38,22 @@ class ApiUser(GrpcLocust):
             stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
             with open('/home/ubuntu/tensorflow-serving-gatling-extension/test.jpg', 'rb') as f:
                 data = f.read()
-                start_time = time.time()
-                request = predict_pb2.PredictRequest()
-                request.model_spec.name = 'inception'
-                request.model_spec.signature_name = 'predict_images'
-                request.inputs['images'].CopyFrom(
-                    tf.contrib.util.make_tensor_proto(data, shape=[1]))
-                try:
+                for _ in range(100):
+                    start_time = time.time()
+                    request = predict_pb2.PredictRequest()
+                    request.model_spec.name = 'inception'
+                    request.model_spec.signature_name = 'predict_images'
+                    request.inputs['images'].CopyFrom(
+                        tf.contrib.util.make_tensor_proto(data, shape=[1]))
+                    try:
                     result = stub.Predict(request, 0.50)  # 10 secs timeout
-                #print(result)
-                except:
-                    total_time = int((time.time() - start_time) * 1000)
-                    print("DDL exceeded")
-                    events.request_failure.fire(request_type="grpc", name='inception', response_time=total_time, exception=e)
-                else:
                     #print(result)
-                    total_time = int((time.time() - start_time) * 1000)
-                    events.request_success.fire(request_type="grpc", name='inception', response_time=total_time, response_length=0)
+                    except:
+                        total_time = int((time.time() - start_time) * 1000)
+                        print("DDL exceeded")
+                        events.request_failure.fire(request_type="grpc", name='inception', response_time=total_time, exception=e)
+                    else:
+                        #print(result)
+                        print("success")
+                        total_time = int((time.time() - start_time) * 1000)
+                        events.request_success.fire(request_type="grpc", name='inception', response_time=total_time, response_length=0)
